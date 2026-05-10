0f5468c1aceb48d81553e442134d581499644fe7
crashyet <adhitya.pan@gmail.com>
2026-05-10 16:19:01 +0700

    second commit

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Sisa Guna') }}</title>
+        <title>Sisa Guna</title>
         <link rel="icon" type="image/png" href="/storage/icon.png">
         <link rel="apple-touch-icon" href="/storage/icon.png">
 
